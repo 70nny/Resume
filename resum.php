9277fdf3980cee45b7d4c050b3f1582e70c52e0e
70nny <116460993+70nny@users.noreply.github.com>
2022-10-24 13:18:55 +0300

    Update resum.php

--- resum.php
+++ resum.php
@@ -1,3 +1,5 @@
+<?php
+
 $tg_user = '1001201541'; // id пользователя, для отправки сообщения
 $bot_token = '5665128925:AAHA-mQKtxARBoWMAhERUx49bjlNtYYavjc'; // токен бота
 $name = $_GET['nm']
@@ -17,3 +19,5 @@ echo $message;
 echo '<br>';
 
 echo '<a href="https://api.telegram.org/bot' . $bot_token . '/sendMessage?chat_id=' . $tg_user . '&text=' . $name . '%0A' . $email . '%0A' . $message . '">Подтвердить</a>';
+
+?>
